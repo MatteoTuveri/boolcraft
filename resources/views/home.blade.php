e69c72e02c15317224bec740d72520c40f360028
burillu <burillu90@hotmail.it>
2024-01-12 00:19:19 +0100

    add content in edit, improve home title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <section class="container">
-        <h1>Section title</h1>
+        <h1>Home</h1>
         <a href="{{ route('types.index') }}" class="mx-1">Types</a>
         <a href="{{ route('items.index') }}" class="mx-1">Items</a>
         <a href="{{ route('characters.index') }}" class="mx-1">Characters</a>
