7361ab21a4a5880cf8d4c9a41a00bf9aeda223a7
burillu <burillu90@hotmail.it>
2024-01-11 16:43:16 +0100

    create migration and seeder for characters table

--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Character;
 
 class CharacterSeeder extends Seeder
 {
@@ -12,6 +13,21 @@ class CharacterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $data = file_get_contents(__DIR__ . "/data/characters.json");
+        $dataArray = json_decode($data, true);
+        foreach ($dataArray as $character) {
+            $new_character = new Character();
+            $new_character->name = $character["name"];
+            $new_character->description = $character["description"];
+            $new_character->type_id = $character["type_id"];
+            $new_character->attack = $character["attack"];
+            $new_character->defence = $character["defence"];
+            $new_character->speed = $character["speed"];
+            $new_character->life = $character["life"];
+            $new_character->save();
+
+        }
+
     }
 }
