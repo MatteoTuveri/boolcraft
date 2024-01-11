1f492c18f0efa025c57d6c2cecad330c679d4234
teoma145 <matteodark145@yahoo.it>
2024-01-11 16:38:42 +0100

    item-migration-and-seeder

--- database/migrations/2024_01_11_144343_create_items_table.php
+++ database/migrations/2024_01_11_144343_create_items_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('name',200);
+            $table->text('description')->nullable();
+            $table->string('slug',200);
+            $table->string('category',100);
+            $table->string('type',100);
+            $table->string('weight',10);
+            $table->string('cost',20);
             $table->timestamps();
         });
     }
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,14 +4,29 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Item;
 
 class ItemSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $json = file_get_contents(__DIR__.'/data/items.json');
+        $items = json_decode($json, true);
+
+        foreach ($items as $itemData) {
+
+            Item::create([
+                'name' => $itemData['name'],
+                'slug' => $itemData['slug'],
+                'category' => $itemData['category'],
+                'type' => $itemData['type'],
+                'weight' => $itemData['weight'],
+                'cost' => $itemData['cost'],
+            ]);
+        }
+
     }
 }
