309a6b4022762d2f8341dde0735c702d58383bdc
burillu <burillu90@hotmail.it>
2024-01-27 14:50:27 +0100

    add stats table, prepare api controller to save game score

---
+++ app/Http/Controllers/Api/StatController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Stat;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class StatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $score_data= $request->all();
+        $validator= Validator::make($score_data,[
+            'name'=>'required',
+            'score'=>'required',
+            
+        ]);
+        if ($validator->fails()){
+            return response()->json([
+                'success'=>false,
+                'errors'=> $validator->errors()
+            ]);
+        };
+        $new_stat = new Stat();
+        $new_stat->fill($form_data);
+        $new_stat->save();
+
+        return response()->json([
+            'success'=>true,
+            'results'=>$new_stat
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Stat $stat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Stat $stat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Stat $stat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Stat $stat)
+    {
+        //
+    }
+}
