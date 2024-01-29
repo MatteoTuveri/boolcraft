ce367c8f3629b2eaf1bf5b7060b490698fd7da76
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-29 11:03:30 +0100

    home page upgraded

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,17 @@
 @endphp
 @extends('layouts.app')
 @section('content')
-    <h1 class="text-center mb-3">Home</h1>
-    <section class="container d-flex justify-content-center">
+    <h1 class="text-center mb-4 mt-2 text-uppercase">Home</h1>
+    <section class="container d-flex gap-3 justify-content-center">
         <div class="mx-1">
-            <div class="card d-flex-column align-items-center" style="width: 18rem;">
-                @if ($type->image)
-                    <img src="{{$baseImgUrl . $type->image }}" class="card-img-top" alt="{{ $type->name }}">
-                @endif
+            <div class="card d-flex-column align-items-center" style="width:18rem;">
+                <div class="home-card">
+                    @if ($type->image)
+                        <img src="{{$baseImgUrl . $type->image }}" class="card-img-top" alt="{{ $type->name }}">
+                        @else
+                        <img src="https://www.worldofleveldesign.com/categories/ue4/images/012-ue4-crash-course-86.jpg" class="card-img-top" alt="{{ $type->name }}">
+                    @endif
+                </div>
 
                 <div class="card-body d-flex-column">
                     <h5 class="card-title text-center">{{ $type->name }}</h5>
@@ -22,9 +26,13 @@
         </div>
         <div class="mx-1">
             <div class="card d-flex-column align-items-center" style="width: 18rem;">
-                @if ($item->image)
-                    <img src="{{ $baseImgUrl . $item->image }}" class="card-img-top" alt="{{ $item->name }}">
-                @endif
+                <div class="home-card">
+                    @if ($item->image)
+                        <img src="{{ $baseImgUrl . $item->image }}" class="card-img-top" alt="{{ $item->name }}">
+                        @else
+                        <img src="https://www.worldofleveldesign.com/categories/ue4/images/012-ue4-crash-course-86.jpg" class="card-img-top" alt="{{ $item->name }}">
+                    @endif
+                </div>
 
                 <div class="card-body d-flex-column">
                     <h5 class="card-title text-center">{{ $item->name }}</h5>
@@ -34,9 +42,13 @@
         </div>
         <div class="mx-1">
             <div class="card d-flex-column align-items-center" style="width: 18rem;">
-                @if ($character->image)
-                    <img src="{{ asset('storage/' . $character->image) }}" class="card-img-top" alt="{{ $character->name }}">
-                @endif
+                <div class="home-card">
+                    @if ($character->image)
+                        <img src="{{ asset('storage/' . $character->image) }}" class="card-img-top" alt="{{ $character->name }}">
+                        @else
+                        <img src="https://www.worldofleveldesign.com/categories/ue4/images/012-ue4-crash-course-86.jpg" class="card-img-top" alt="{{ $character->name }}">
+                    @endif
+                </div>
                 <div class="card-body d-flex-column">
                     <h5 class="card-title text-center">{{ $character->name }}</h5>
                     <a href="{{ route('admin.characters.index') }}" class="btn btn-primary">Go to characters</a>
